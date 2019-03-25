0de7be2b08d2bdbbad30d4a13b0a316a9d269dc4
VinTran <tranquillo.vinnie@gmail.com>
2019-03-25 19:28:15 -0400

    Spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
+
 var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+var search = process.argv[2]; 
+
+spotify.search({ type: 'track', query: search }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
