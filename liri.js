a189757f88d26d1cb550fad3e915f33c4a9c83e8
VinTran <tranquillo.vinnie@gmail.com>
2019-03-29 14:07:49 -0400

    Final Commit

--- liri.js
+++ liri.js
@@ -3,44 +3,45 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var moment = require("moment");
-let fs = require("fs");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var userOption = process.argv[2];
-var inputParameter = process.argv[3];
+var input = process.argv[3];
 
-UserInputs(userOption, inputParameter);
+UserInputs(userOption, input);
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-function UserInputs(userOption, inputParameter) {
+function UserInputs(userOption, input) {
   switch (userOption) {
     case 'concert-this':
-      showConcert(inputParameter);
+      showConcert(input);
       break;
     case 'spotify-this-song':
-      showSongInfo(inputParameter);
+      showSongInfo(input);
       break;
     case 'movie-this':
-      showMovieInfo(inputParameter);
+      showMovieInfo(input);
       break;
     case 'do-what-it-says':
-      doWhatItSay();
+      doWhat();
       break;
     default:
-      console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+      console.log("Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
   }
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-function showConcert(inputParameter) {
-  axios.get("https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp")
+function showConcert(input) {
+  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
     .then(function (response) {
-      for (var i = 0; i < 5; i++) {
-        var concertResults = "-----------------------------------------------------" +
+      for (var i = 0; i < 5; i++) { //limited results to 5 because there were a lot 
+        var showConcertResults = "-----------------------------------------------------" +
           "\nVenue Name: " + response.data[i].venue.name +
           "\nVenue Location: " + response.data[i].venue.city +
           "\nDate of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY") +
           "\n--------------------------------------------------------------------";
-        console.log(concertResults);
+        console.log(showConcertResults);
+        fs.appendFileSync("log.txt", showConcertResults); 
       }
     })
     .catch(function (error) {
@@ -49,14 +50,14 @@ function showConcert(inputParameter) {
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-function showSongInfo(inputParameter) {
-  if (inputParameter === undefined) {
-    inputParameter = "The Sign"; //default Song
+function showSongInfo(input) {
+  if (input === undefined) {
+    input = "The Sign"; //default Song
   }
   spotify.search(
     {
       type: "track",
-      query: inputParameter
+      query: input
     },
     function (err, data) {
       if (err) {
@@ -66,42 +67,52 @@ function showSongInfo(inputParameter) {
       var songs = data.tracks.items;
 
       for (var i = 0; i < songs.length; i++) {
-        console.log("**********SONG INFO*********");
-        console.log(i);
-        console.log("Song name: " + songs[i].name);
-        console.log("Preview song: " + songs[i].preview_url);
-        console.log("Album: " + songs[i].album.name);
-        console.log("Artist(s): " + songs[i].artists[0].name);
-        console.log("*****************************");
+        var spotifyResult = "------------SONG-----------------------" +
+          "\n" + (i) +
+          "\nSong name: " + songs[i].name +
+          "\nPreview song: " + songs[i].preview_url +
+          "\nAlbum: " + songs[i].album.name +
+          "\nArtist: " + songs[i].artists[0].name +
+          "\n---------------------------------------"
+        console.log(spotifyResult);
+        fs.appendFileSync("log.txt", spotifyResult); 
+
       }
     }
   );
 };
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-function showMovieInfo(inputParameter) {
-  if (inputParameter === undefined) {
-    inputParameter = "Mr. Nobody"
-    console.log("-----------------------");
-    console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-    console.log("It's on Netflix!");
+function showMovieInfo(input) {
+  if (input === undefined) {
+    input = "Mr. Nobody"
+    var mrNob = "-----------------------" +
+      "\nIf you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +
+      "\nIt's on Netflix!"
+    console.log(mrNob);
+    fs.appendFileSync("log.txt", mrNob); 
+
   }
-  axios.get("http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=trilogy").then(
+  axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy").then(
     function (response) {
-      console.log("**********MOVIE INFO*********");
-      console.log("Title: " + response.data.Title);
-      console.log("Release Year: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.imdbRating);
-      //console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies));
-      console.log("Country of Production: " + response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
-      console.log("*****************************");
+      var movRez = "--------------MOVIE---------------" +
+        "\nTitle: " + response.data.Title +
+        "\nRelease Year: " + response.data.Year +
+        "\nIMDB Rating: " + response.data.imdbRating +
+        //console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value ); couldnt get rotten tomatoes to work for some reason
+        "\nCountry of Production: " + response.data.Country +
+        "\nLanguage: " + response.data.Language +
+        "\nPlot: " + response.data.Plot +
+        "\nActors: " + response.data.Actors +
+        "\n-----------------------------"
+        console.log(movRez);
+        fs.appendFileSync("log.txt", movRez); 
     });
 }
+
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-function doWhatItSay() {
+function doWhat() {
   fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
       return console.log(error);
